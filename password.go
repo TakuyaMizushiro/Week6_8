44ac8d07a1fa7c303190cf685a1075f425e804c5
Takuya Mizushiro <m213251@hiroshima-u.ac.jp>
2021-07-21 07:57:51 +0000

    modify files

--- password.go
+++ password.go
@@ -1,45 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "unicode/utf8"
+	"fmt"
+	"strings"
+	"unicode/utf8"
 )
-
-//func UpperCase(x string) string { //大文字があるか判定
-
-//}
-
-//func LowerCase(x string) string { //小文字があるか判定
-
-//}
 
 func LengthEight(x string) bool { //8文字以上か判定
 
-    if(utf8.RuneCountInString(ja) >= 8) {
-    	return true
-    }
-    else {
-    	return false
-    }
+	if utf8.RuneCountInString(x) >= 8 {
+		return true
+	} else {
+		return false
+	}
+
 }
 
-//func Number(x string) string { //数字が入っているか判定
-
-//}
-
-//func Symbol(x string) string { //記号が入っているか判定
-
-//}
+func checkCapitalLetter(str string) bool { //大文字のアルファベットが入力されているかチェック
+	if "A" <= str && str <= "Z" {
+		//fmt.Println("Large Capital OK")
+		return true
+	} else {
+		return false
+	}
+}
 
 func main() {
 	var s string
 
-	fmt.Print("Input your password: ")
-	fmt.Scanf("%d", &s)
+	var checkEight bool = false
+	var largeCap bool = false
 
-	//UpperCase(s)
-	//LowerCase(s)
-	LengthEight(s)
-	//Number(s)
-	//Symbol(s)
+	fmt.Print("Input your password: ")
+	fmt.Scan(&s)
+
+	//文字列をスライスに一文字ずつ代入
+	arr := strings.Split(s, "")
+
+	//スライスの要素をひとつずつ取り出して、最後の要素までfor文を回す
+	//largeCap：大文字が入っているか判断
+	for _, str := range arr {
+		if largeCap == false && checkCapitalLetter(str) {
+			largeCap = true
+		}
+	}
+	//八文字以上か確認、あるならtrue
+	checkEight = LengthEight(s)
+
+	//すべての項目について確認し、安全性を標準出力
+	if checkEight == true && largeCap == true {
+		//secure = true
+		fmt.Println("入力された文字列は安全です")
+	} else {
+		fmt.Println("以下の理由により入力された文字列は安全ではありません")
+		if checkEight == false {
+			fmt.Println("・8文字以上のパスワードを入力してください")
+		}
+		if largeCap == false {
+			fmt.Println("・大文字のアルファベットが含まれていません")
+		}
+	}
+
 }
--- password_test.go
+++ password_test.go
@@ -6,10 +6,10 @@ import (
 	"testing"
 )
 
-func TestStringReturn01(t *testing.T) {
+/*func TestStringReturn01(t *testing.T) {
 	//出力確認
 	main()
-}
+}*/
 
 func TestCount01(t *testing.T) {
 	//８文字以上かどうかの確認(8文字以上)
@@ -26,5 +26,23 @@ func TestCount02(t *testing.T) {
 	s = "aisy98!"
 	if LengthEight(s) == true {
 		t.Error("TestCount02 is failed")
+	}
+}
+
+func TestCount03(t *testing.T) {
+	//大文字が含まれているかの確認（大文字あり）
+	var s string
+	s = "Aisy98!"
+	if checkCapitalLettergit(s) != true {
+		t.Error("TestCount03 is failed")
+	}
+}
+
+func TestCount04(t *testing.T) {
+	//大文字が含まれているかの確認（大文字なし）
+	var s string
+	s = "aisy98!"
+	if checkCapitalLetter(s) == true {
+		t.Error("TestCount04 is failed")
 	}
 }
